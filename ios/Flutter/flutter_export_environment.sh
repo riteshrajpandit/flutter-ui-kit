8d23e1e5b58683c6fe969cd6007b18e23cd387ea
Ritesh Raj Pandit <41156634+riteshrajpandit@users.noreply.github.com>
2024-05-28 11:02:11 +0545

    internal updates

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.22.0/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/riteshrajpandit/Documents/materialx_flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/riteshrajpandit/Documents/GitHub/flutter-ui-kit"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/riteshrajpandit/Documents/GitHub/flutter-ui-kit/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=2.3.0"
 export "FLUTTER_BUILD_NUMBER=230"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNjM0NGI3NWRjZjg2MWQ4YmYxZjEzMjI3ODBiODgxMWY5ODJlMzFhLw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/riteshrajpandit/Documents/GitHub/flutter-ui-kit/.dart_tool/package_config.json"
